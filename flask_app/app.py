b9130d0d664663258474177426b917b8a0db2339
kathytran88 <kathyngananhtran@gmail.com>
2024-08-23 16:47:04 -0400

    logistic.pkl

--- flask_app/app.py
+++ flask_app/app.py
@@ -7,18 +7,21 @@ import pandas as pd
 
 app = Flask(__name__)
 
-import os
-
 # Absolute path to the file
 file_path = os.path.join(os.path.dirname(__file__), 'logistic.pkl')
+print(f"Loading model from: {file_path}")
 
-with open(file_path, 'rb') as file:
-    logistic_model = pickle.load(file)
+try:
+    with open(file_path, 'rb') as file:
+        model = pickle.load(file)
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+    model = None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     prediction_text = None
-    
+
     # Handle the form submission for the Logistic Regression model
     if request.method == 'POST':
         age = float(request.form['age'])
@@ -26,34 +29,38 @@ def index():
 
         input_data = np.array([[age, balance]])
 
+        # Assuming you have a scaler saved; otherwise, this step should be adapted
         scaler = StandardScaler()
         input_data = scaler.fit_transform(input_data)
 
-        prediction = logistic_model.predict(input_data)
+        if model is not None:
+            prediction = model.predict(input_data)
+            prediction_text = 'Yes (will subscribe to term deposit)' if prediction[0] == 1 else 'No (will not subscribe to term deposit)'
+        else:
+            prediction_text = 'Model not found'
 
-        prediction_text = 'Yes (will subscribe to term deposit)' if prediction[0] == 1 else 'No (will not subscribe to term deposit)'
-    
     # Load the SQL file content
-    path_to_file = '../sql_analysis.sql'
+    path_to_file = 'sql_analysis.sql'  # Adjust the path if necessary
     sql_content = None
     if os.path.exists(path_to_file):
         with open(path_to_file, 'r') as file:
             sql_content = file.read()
     else:
         sql_content = "File inaccessible."
-    
+
     # Load csv file 
-    path_to_csv = '../SQL_results.csv'
+    path_to_csv = 'SQL_results.csv'  # Adjust the path if necessary
     csv_content = None
     if os.path.exists(path_to_csv):
         df = pd.read_csv(path_to_csv)
         df.columns = df.columns.str.strip()
-        df = df.rename(columns = {'Unnamed 0': ''})
+        df = df.rename(columns={'Unnamed 0': ''})
         csv_content = df.to_html(classes='csv-table', index=False)
     else:
         csv_content = 'File inaccessible'
+
     # Render the template with both prediction and SQL content
-    return render_template('index.html',sql_content = sql_content, prediction_text=prediction_text, csv_content = csv_content)
+    return render_template('index.html', sql_content=sql_content, prediction_text=prediction_text, csv_content=csv_content)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
