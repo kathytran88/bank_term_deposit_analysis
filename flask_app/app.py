9617c949424b3cf631998958c8a341e127ff6ec9
kathytran88 <kathyngananhtran@gmail.com>
2024-08-23 10:54:03 -0400

    flask_app

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+import os
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,7 +12,9 @@ with open('logistic.pkl', 'rb') as file:
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    prediction_text = None 
+    prediction_text = None
+    
+    # Handle the form submission for the Logistic Regression model
     if request.method == 'POST':
         age = float(request.form['age'])
         balance = float(request.form['balance'])
@@ -23,4 +27,28 @@ def index():
         prediction = logistic_model.predict(input_data)
 
         prediction_text = 'Yes (will subscribe to term deposit)' if prediction[0] == 1 else 'No (will not subscribe to term deposit)'
-    return render_template('index.html', prediction_text = prediction_text)
+    
+    # Load the SQL file content
+    path_to_file = '/Users/nguyenthang/Downloads/kathy/bank_term_deposit_analysis/sql_analysis.sql'
+    sql_content = None
+    if os.path.exists(path_to_file):
+        with open(path_to_file, 'r') as file:
+            sql_content = file.read()
+    else:
+        sql_content = "File inaccessible."
+    
+    # Load csv file 
+    path_to_csv = '/Users/nguyenthang/Downloads/kathy/bank_term_deposit_analysis/SQL_results.csv'
+    csv_content = None
+    if os.path.exists(path_to_csv):
+        df = pd.read_csv(path_to_csv)
+        df.columns = df.columns.str.strip()
+        df = df.rename(columns = {'Unnamed 0': ''})
+        csv_content = df.to_html(classes='csv-table', index=False)
+    else:
+        csv_content = 'File inaccessible'
+    # Render the template with both prediction and SQL content
+    return render_template('index.html',sql_content = sql_content, prediction_text=prediction_text, csv_content = csv_content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
